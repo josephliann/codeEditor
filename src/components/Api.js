96b20c609bf36f5243b3fac256df21515a2fd587
lian suan mang <joseph.lian089@gmail.com>
2026-06-19 16:58:44 +0530

    Update Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: ""http://https//codeeditor-anwu.onrender.com/run"",
+  baseURL: "http://https//codeeditor-anwu.onrender.com/run",
 });
 
 const compilerMap = {
