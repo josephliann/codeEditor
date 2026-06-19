12574673b3384c2e92787a9486a570492290ec77
lian suan mang <joseph.lian089@gmail.com>
2026-06-19 16:48:47 +0530

    Update Codeeditor.jsx

--- src/components/Codeeditor.jsx
+++ src/components/Codeeditor.jsx
@@ -23,7 +23,7 @@ function CodeEditor() {
     setShowDebugger(true);
 
     try {
-      const response = await fetch("http://localhost:3000/debug", {
+      const response = await fetch("https://codeeditor-anwu.onrender.com/debug", {
         method: "POST",
 
         headers: {
